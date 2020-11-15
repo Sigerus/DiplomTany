b4d1f7eb172955f822ab3b106725ab821be9e34a
Siger <Sigerus1990@gmail.com>
2020-11-16 00:53:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/axelerometr/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.axelerometr;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    TextView tvText;
+    TextView msText;
+    Button startlog;
+    Button stoplog;
+    SensorManager sensorManager;
+    Sensor sensorAccel;
+    Sensor sensorLinAccel;
+    Sensor sensorGravity;
+
+    final String LOG_TAG = "myLogs";
+    final String FILENAME = "file";
+    final String DIR_SD = "MyFiles";
+    final String FILENAME_SD = "fileSD";
+
+    StringBuilder sb = new StringBuilder();
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+        tvText = findViewById(R.id.tvText);
+        msText = findViewById(R.id.zolupa);
+        startlog = findViewById(R.id.startlog);
+        stoplog = findViewById(R.id.stoplog);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorLinAccel = sensorManager
+                .getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        startlog.setOnClickListener(this);
+        stoplog.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, sensorAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorLinAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorGravity,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showInfo();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 400);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+        timer.cancel();
+    }
+
+    String format(float values[]) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1],
+                values[2]);
+    }
+
+    void showInfo() {
+        sb.setLength(0);
+        sb.append("Accelerometer: " + format(valuesAccel))
+                .append("\n\nAccel motion: " + format(valuesAccelMotion))
+                .append("\nAccel gravity : " + format(valuesAccelGravity))
+                .append("\n\nLin accel : " + format(valuesLinAccel))
+                .append("\nGravity : " + format(valuesGravity));
+        tvText.setText(sb);
+    }
+
+    float[] valuesAccel = new float[3];
+    float[] valuesAccelMotion = new float[3];
+    float[] valuesAccelGravity = new float[3];
+    float[] valuesLinAccel = new float[3];
+    float[] valuesGravity = new float[3];
+
+    SensorEventListener listener = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i = 0; i < 3; i++) {
+                        valuesAccel[i] = event.values[i];
+                        valuesAccelGravity[i] = (float) (0.1 * event.values[i] + 0.9 * valuesAccelGravity[i]);
+                        valuesAccelMotion[i] = event.values[i]
+                                - valuesAccelGravity[i];
+                    }
+                    break;
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+                    for (int i = 0; i < 3; i++) {
+                        valuesLinAccel[i] = event.values[i];
+                    }
+                    break;
+                case Sensor.TYPE_GRAVITY:
+                    for (int i = 0; i < 3; i++) {
+                        valuesGravity[i] = event.values[i];
+                    }
+                    break;
+            }
+
+        }
+
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.startlog:
+                msText.setText("Текстовый файл создан\nИдёт запись логов");
+                writeFileSD();
+            break;
+            case R.id.stoplog:
+                msText.setText("Логи успешно записаны");
+            break;
+        }
+    }
+    void writeFileSD() {
+        // проверяем доступность SD
+        if (!Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        // создаем каталог
+        sdPath.mkdirs();
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(sdPath, FILENAME_SD);
+        try {
+            // открываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new FileWriter(sdFile));
+            // пишем данные
+            bw.write("Содержимое файла на SD");
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан на SD: " + sdFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
