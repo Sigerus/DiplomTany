6049d8e2f46ef1acead1155d9a2df7d3825eb475
anton <anton.boev00@mail.ru>
2020-11-25 16:23:13 +0300

    save log 1 string

--- app/src/main/java/com/example/axelerometr/MainActivity.java
+++ app/src/main/java/com/example/axelerometr/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.axelerometr;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.WebSocket;
 
@@ -27,14 +33,22 @@ public class MainActivity extends AppCompatActivity {
     Button saveButton;
     final String TAG = "ACTIVITY_STATEecc";
     String A = "pizdec";
+    private String text;
+    private String[] text2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        start_btn=findViewById(R.id.button_start);
-        stop_btn=findViewById(R.id.button_stop);
-     Connect();
+        start_btn = findViewById(R.id.button_start);
+        stop_btn = findViewById(R.id.button_stop);
+
+        Connect();
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            saveButton.setEnabled(false);
+        } else {
+            myExternalFile = new File(getExternalFilesDir(filepath), filename);
+        }
        /* if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
             saveButton.setEnabled(false);
         }
@@ -43,30 +57,70 @@ public class MainActivity extends AppCompatActivity {
         }
   //  Log.d(TAG, "Connect");*/
 
-Log.d(TAG, String.valueOf(sm));
+        Log.d(TAG, String.valueOf(_ws));
+        // settext( text);
 
-    /*    stop_btn.setOnClickListener(new View.OnClickListener() {
+        start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // settext(text);
                 try {
+                   /* for (int i = 2; i < 30; i++) {
+                        text2[i] = text;
+                    }*/
                     FileOutputStream fos = new FileOutputStream(myExternalFile);
-                    fos.write((json).getBytes());
+                    //fos.write(text).getBytes());
+
+                    //  fos.write(text.getBytes( ));
+
+
+
+                        fos.write((text + "\n").getBytes());
+                    
+
                     fos.close();
-                    Toast.makeText(MainActivity.this,"Log stat",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Log created", Toast.LENGTH_LONG).show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+
             }
-        });*/
+        });
+
+        WebSocketGolos.getMeasurementsCallBack = this::settext;
+
+        stop_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text2[0]="0";
+                if(text!=null){
+                for (int i=0;i<30;i++){
+
+                    text2[i]=text;
+                    }
+                }
+            }
+        });
 
     }
+
+
+
+    public void settext(String text) {
+
+
+        this.text = text;
+        // text2[i]=text;
+    }
+
+
     public void Connect() {
         sm = new SocketManager();
         _ws = sm.Connect();
     }
 
-  /*  private boolean isExternalStorageReadOnly() {
+   private boolean isExternalStorageReadOnly() {
         String extStorageState = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
             return true;
@@ -81,12 +135,10 @@ Log.d(TAG, String.valueOf(sm));
         }
         return false;
     }
-*/
+
 
     //  final byte[] ipAddr = new byte[]{(byte)176, (byte)15, (byte)174, (byte)137};
 
 
-
-    
 
 }
--- app/src/main/java/com/example/axelerometr/WebSocketGolos.java
+++ app/src/main/java/com/example/axelerometr/WebSocketGolos.java
@@ -1,6 +1,15 @@
 package com.example.axelerometr;
 
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import okhttp3.Response;
 import okhttp3.WebSocket;
@@ -12,7 +21,6 @@ import okhttp3.WebSocketListener;
 
 public class WebSocketGolos extends WebSocketListener {
 
-    final String TAG2 = "LOGVAAsfASf";
     final String TAG = "ACTIVITY_log";
     /*private Color parseColor(String Message){
         int Red = Integer.parseInt(Message.split(",")[0]);
@@ -20,6 +28,15 @@ public class WebSocketGolos extends WebSocketListener {
         int Blue = Integer.parseInt(Message.split(",")[2]);
         return new Color(Red/255f,Green/255f,Blue/255f,1);
     }*/
+
+
+    public static MyCallBack getMeasurementsCallBack;
+   // private SatteliteMeasurement[];
+
+    public interface MyCallBack{
+        void callBackCall( String text );
+    }
+
 
     WebSocketGolos()
     {
@@ -33,118 +50,15 @@ public class WebSocketGolos extends WebSocketListener {
     {
         String[] Mass = text.split(":");
         String Code = Mass[0];
-
         String[] Message = {};
         if (Mass.length > 1) {
             Message = Mass[1].split(" ");
         }
+
         Log.d(TAG, text);
-       // Log.d(TAG, Message[10]);
-       /* Log.d(TAG, Message[105]);
-        Log.d(TAG, Message[150]);
-        Log.d(TAG, Message[200]);
-        Log.e(TAG2, String.valueOf(Message));
-        Log.i(TAG, String.valueOf(Message));
-        Log.v(TAG, String.valueOf(Message));
-        Log.w(TAG, String.valueOf(Mass));*/
-       // String Guid;
-       // Color color;
-       /* switch (Code){
-            case "id":
-                Game.Me.Connected = true;
-                Guid = Message[0];
-                color = parseColor(Message[1]);
-                int MyIndex= Integer.parseInt(Message[2]);
-                Game.Me.SetGUIDAndColor(Guid,color,MyIndex);
-                break;
-            case "NewPlayer":
-                Guid = Message[0];
-                color = parseColor(Message[1]);
-                String Nick = Message[2];
-                int NewPlayerIndex = Integer.parseInt(Message[3]);
-                Game.AddPlayer(Guid, color, Nick,NewPlayerIndex);
-                Game.Me.Connected = true;
-                break;
-            case "Throw":
-                Guid = Message[0];
 
-                //int Color = Integer.parseInt(Message[1]);
 
-                //Касательная
-                float x = Float.parseFloat(Message[2]);
-                float y = Float.parseFloat(Message[3]);
-                float z = Float.parseFloat(Message[4]);
-
-                float tanx = Float.parseFloat(Message[5]);
-                float tany = Float.parseFloat(Message[6]);
-                float tanz = Float.parseFloat(Message[7]);
-
-                long mills = Game.time;
-                Player player = Game.FindPlayerByGuid(Guid);
-                if (player != null)
-                    player.ThrowBall(x, y, z, tanx, tany, tanz, mills);
-                Game.Me.Connected = true;
-                break;
-            case "Close":
-                Guid = Message[0];
-                Game.FindPlayerByGuid(Guid).Close();
-                Game.Me.CloseWithoutMessage();
-                Game.Stop();
-                break;
-            case "ConnectionLost":
-                Guid = Message[0];
-                Game.FindPlayerByGuid(Guid).ConnectionLost();
-                Game.Stop();
-                break;
-            case "ConnectionFind":
-                Guid = Message[0];
-                Game.FindPlayerByGuid(Guid).ReConnect();
-                break;
-            case "Start":
-                Color Needcolor = parseColor(Message[0]);
-                Game.StartGame(Needcolor,parseColor(Message[1]));
-                break;
-            case "Check":
-                Game.Me.Answer();
-                synchronized (Game.Me.locker) {
-                    Game.Me.Connected = true;
-                }
-                break;
-            case "Stop":
-                Game.Stop();
-                break;
-            case "NewLickerColor":
-                Game.licker.SetNewNeedColor(parseColor(Message[0]));
-                Game.Score++;
-                break;
-            case "NewPlayerColor":
-                Guid = Message[0];
-                Game.FindPlayerByGuid(Guid).SetColor(parseColor(Message[1]));
-                break;
-            case "NewCurrentColor":
-                Color cl = parseColor(Message[0]);
-                Game.licker.SetCurrentColor(cl);
-                break;
-            case "Error":
-                Game.Me.Connected = false;
-                String ErrorMessage = Message[0];
-                Game.NonConnectReason = ErrorMessage;
-                Game.Me.CloseWithoutMessage();
-                break;
-            case "Ready":
-                Guid = Message[0];
-                boolean ready = Boolean.getBoolean(Message[1]);
-                Game.ChangePlayerStatus(Guid,ready);
-                break;
-            case "TimeLeft":
-                Game.Time = Integer.parseInt(Message[0]);
-                break;
-            case "TimeToGo":
-                Game.TimeToGo = Integer.parseInt(Message[0]);
-                break;
-            case "Go":
-                Game.Start = true;
-                break;
-        }*/
+        getMeasurementsCallBack.callBackCall(text);
     }
+
 }
